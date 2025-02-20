61445606684c28e21db3b512916761384196e383
z7md <z7mdd90@gmail.com>
2025-02-20 09:46:42 +0300

    add layouts and html files

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
- path('', views.index),
- path('index2/<int:val1>/', views.index2),
- path('<int:bookId>', views.viewbook)
+ path('', views.index, name= "books.index"),
+ path('list_books/', views.list_books, name= "books.list_books"),
+ path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+ path('aboutus/', views.aboutus, name="books.aboutus"),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -17,3 +17,12 @@ def viewbook(request, bookId):
  if book2['id'] == bookId: targetBook = book2
  context = {'book':targetBook} # book is the variable name accessible by the template
  return render(request, 'bookmodule/show.html', context)
+
+def index(request):
+ return render(request, "bookmodule/index.html")
+def list_books(request):
+ return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+ return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
